1babba992366dd237c84b97e897d893a0075a920
Lucas <lukaspgtu14@gmaill.com>
2020-04-14 04:24:26 -0300

    up

--- app/Product.php
+++ app/Product.php
@@ -30,23 +30,17 @@ class Product extends Model
         'status' => 1
     ];
 
-    public function uploadPhoto($photo)
+    public function uploadPhoto($file)
     {
         $name = uniqid(date('HisYmd'));
 
-        $image_parts = explode(";base64,", $photo);
+        $ext = $file->extension();
 
-        $image_type_aux = explode("image/", $image_parts[0]);
+        $full_name = "{$name}.{$ext}";
 
-        $type = $image_type_aux[1];
+        $file->storeAs('products', $full_name);
 
-        $full_name = "{$name}.{$type}";
-
-        $image = base64_decode($image_parts[1]);
-
-        Storage::put("products/$full_name", $image);
-
-        $this->photo = env('APP_URL') . "storage/products/$full_name";
+        $this->photo = $full_name;
 
         $this->save();        
     }
