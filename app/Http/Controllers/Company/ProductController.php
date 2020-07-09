038a8481156f90a65e6a34222c1a8f34980230f8
Lucas <lukaspgtu14@gmaill.com>
2020-07-09 17:05:45 -0300

    up

--- app/Http/Controllers/Company/ProductController.php
+++ app/Http/Controllers/Company/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::select('id', 'menu_session_id', 'photo', 'name', 'description')
+        $products = Product::select('id', 'menu_session_id', 'photo', 'name', 'status', 'description')
             ->where('company_id', Auth::id())
             ->orderBy('name', 'asc')
             ->get();
