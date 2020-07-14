4d167647970f8d4280c3598c28b88100854f0a22
Lucas <lukaspgtu14@gmaill.com>
2020-07-14 18:07:25 -0300

    up

---
+++ database/migrations/2020_07_14_153845_create_banks.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBanks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+
+            $table->smallIncrements('id');
+
+            $table->unsignedSmallInteger('code');
+
+            $table->string('name', 100);
+
+            $table->boolean('status');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
